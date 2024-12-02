927120f9c1dd701c32dffa9780087f88ab09a900
Filip <filip.michalski.1@student.put.poznan.pl>
2024-12-02 12:25:09 +0100

    drivers

--- src/signal_operations/signal_operations/subscriber_function.py
+++ src/signal_operations/signal_operations/subscriber_function.py
@@ -3,6 +3,8 @@ from rclpy.node import Node
 
 from ad_ws_interfaces.msg import Xsens
 from geometry_msgs.msg import Vector3Stamped
+from geometry_msgs.msg import PoseStamped
+from control_interfaces.msg import ServoState
 
 
 class XsensSubscriber(Node):
@@ -10,8 +12,23 @@ class XsensSubscriber(Node):
     def __init__(self):
         super().__init__('xsens_subscriber')
         self.subscription = self.create_subscription(
-            Vector3Stamped,
-            'imu/angular_velocity',
+            PoseStamped,
+            'optitrack/rigid_body_0',
+            self.listener_callback,
+            10)
+        self.subscription 
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Angular velocity x: "%f"'% msg.vector.x)
+        self.get_logger().info('Angular velocity y: "%f"'% msg.vector.y)
+        self.get_logger().info('Angular velocity z: "%f"'% msg.vector.z)
+
+class ServoSubscriber(Node):
+    def __init__(self):
+        super().__init__('servo_subscriber')
+        self.subscription = self.create_subscription(
+            ServoState,
+            'pub_position',
             self.listener_callback,
             10)
         self.subscription 
@@ -22,17 +39,21 @@ class XsensSubscriber(Node):
         self.get_logger().info('Angular velocity z: "%f"'% msg.vector.z)
 
 
+
 def main(args=None):
     rclpy.init(args=args)
 
     xsens_subscriber = XsensSubscriber()
+    servo_subscriber = XsensSubscriber()
 
     rclpy.spin(xsens_subscriber)
+    rclpy.spin(servo_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
     xsens_subscriber.destroy_node()
+    servo_subscriber.destroy_node()
     rclpy.shutdown()
 
 
