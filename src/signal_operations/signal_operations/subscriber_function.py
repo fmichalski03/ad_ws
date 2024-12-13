bc94df6db51ce3571affa33fcfbd1855d29cf301
piotr <piotr.las@protonmail.com>
2024-12-13 23:38:31 +0100

    update subscriber_function - add writing read data to database

--- src/signal_operations/signal_operations/subscriber_function.py
+++ src/signal_operations/signal_operations/subscriber_function.py
@@ -1,14 +1,21 @@
 import rclpy
 from rclpy.node import Node
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+from dotenv import load_dotenv
+import os
 
 from control_interfaces.msg import ServoState
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 from vesc_msgs.msg import VescStateStamped
 
-
 class ServoSubscriber(Node):
-    def __init__(self):
+    def __init__(self, write_api, bucket, org):
         super().__init__('servo_subscriber')
+
+        self.write_api = write_api
+        self.bucket = bucket
+        self.org = org
 
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
@@ -24,9 +31,18 @@ class ServoSubscriber(Node):
         )
 
     def listener_callback(self, msg):
+        point = (
+            influxdb_client.Point("my_measurement")
+            .field("position", msg.position)
+            .field("velocity", msg.velocity)
+            .field("torque", msg.torque)
+        )
+        self.write_api.write(bucket=self.bucket, org=self.org, record=point)
+
         self.get_logger().info('Position: "%f"' % msg.position)
         self.get_logger().info('Velocity: "%f"' % msg.velocity)
-        self.get_logger().info('Torque: "%f"' % msg.torque) 
+        self.get_logger().info('Torque: "%f"' % msg.torque)
+
 
 class StateSubscriber(Node):
     def __init__(self):
@@ -48,13 +64,28 @@ class StateSubscriber(Node):
     def listener_callback(self, msg):
         self.get_logger().info('Motor current: "%f"' % msg.state.current_motor)
         self.get_logger().info('Speed: "%f"' % msg.state.speed)
-        self.get_logger().info('Voltage input: "%f"' % msg.state.voltage_input) 
+        self.get_logger().info('Voltage input: "%f"' % msg.state.voltage_input)
 
 
 def main(args=None):
+    load_dotenv()
+
+    # In root of the project, create a .env file with the following variables:
+    bucket = os.getenv("INFLUXDB_BUCKET")
+    org = os.getenv("INFLUXDB_ORG")
+    token = os.getenv("INFLUXDB_TOKEN")
+    url = os.getenv("INFLUXDB_URL")
+
+    client = influxdb_client.InfluxDBClient(
+        url=url,
+        token=token,
+        org=org
+    )
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
     rclpy.init(args=args)
 
-    servo_subscriber = ServoSubscriber()
+    servo_subscriber = ServoSubscriber(write_api, bucket, org)
     state_subscriber = StateSubscriber()
 
     rclpy.spin(servo_subscriber)
