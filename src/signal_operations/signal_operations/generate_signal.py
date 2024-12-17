c5df6aaa3f3a19c63546a5a82c93908d6b39d00f
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2024-12-17 21:41:03 +0100

    new_signal_gen

--- src/signal_operations/signal_operations/generate_signal.py
+++ src/signal_operations/signal_operations/generate_signal.py
@@ -1,9 +1,11 @@
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 from scipy.interpolate import splrep, splev
 
 def generate_control_points(n_points=20, length=1000, value_range=(-1, 1)):
     # Losowe punkty kontrolne
+    np.random.seed(random.randint(1,1000))
     x = np.linspace(0, 1, n_points)
     y = np.random.uniform(value_range[0], value_range[1], n_points)
     
@@ -12,4 +14,4 @@ def generate_control_points(n_points=20, length=1000, value_range=(-1, 1)):
     x_new = np.linspace(0, 1, length)
     y_new = splev(x_new, tck)
 
-    return x_new, y_new
+    return y_new
--- src/signal_operations/signal_operations/publisher_function.py
+++ src/signal_operations/signal_operations/publisher_function.py
@@ -7,18 +7,6 @@ import numpy as np
 from signal_operations.generate_signal import generate_control_points
 
 
-def generate_signal():
-    f = 5         # częstotliwość w Hz
-    fs = 500      # częstotliwość próbkowania w Hz
-    t = np.arange(0, 1, 1/fs)  # przedział czasu od 0 do 1 sekundy
-
-    # Generowanie sygnału sinusoidalnego
-    amplitude = 0.5  # amplituda sygnału
-    signal = amplitude * np.sin(2 * np.pi * f * t)
-    signal_array = signal.tolist()
-    return signal_array
-
-signal_array = generate_signal()
 
 class SignalPublisher(Node):
 
@@ -26,18 +14,24 @@ class SignalPublisher(Node):
         super().__init__('singal_publisher')
         self.publisher_ = self.create_publisher(Control, 'commands/ctrl', 10)
         timer_period = 0.01  # seconds
+        self.steering_array = generate_control_points(value_range=(-0.5,0.5))
+        self.speed_array = generate_control_points(value_range=(0.5,4))
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = Control()
         # -0.5 to 0.5
-        msg.steering_angle = signal_array[self.i]
+        msg.steering_angle = self.steering_array[self.i]
+        msg.set_speed = self.speed_array[self.i]
         self.i+=1
-        if self.i == len(signal_array):
+        if self.i == len(self.steering_array):
+            self.steering_array = generate_control_points(value_range=(-0.5,0.5))
+            self.speed_array = generate_control_points(value_range=(0,4))
             self.i=0
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%f"' % msg.steering_angle)
+        self.get_logger().info('Publishing steering: "%f"' % msg.steering_angle)
+        self.get_logger().info('Publishing speed: "%f"' % msg.set_speed)
 
 
 
