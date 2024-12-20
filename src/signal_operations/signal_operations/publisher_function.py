ed4d8df62ec363cbb8a67ceec7ae69892ac42406
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2024-12-20 12:43:47 +0100

    test_signals

--- src/signal_operations/signal_operations/publisher_function.py
+++ src/signal_operations/signal_operations/publisher_function.py
@@ -12,7 +12,7 @@ class SignalPublisher(Node):
 
     def __init__(self):
         super().__init__('singal_publisher')
-        self.publisher_ = self.create_publisher(Control, 'commands/ctrl', 10)
+        self.publisher_ = self.create_publisher(Control, '/mpc/ctrl', 10)
         timer_period = 0.01  # seconds
         self.steering_array = generate_control_points(value_range=(-0.5,0.5))
         self.speed_array = generate_control_points(value_range=(0.5,4))
@@ -22,12 +22,13 @@ class SignalPublisher(Node):
     def timer_callback(self):
         msg = Control()
         # -0.5 to 0.5
+        msg.control_mode = msg.SPEED_MODE
         msg.steering_angle = self.steering_array[self.i]
         msg.set_speed = self.speed_array[self.i]
         self.i+=1
         if self.i == len(self.steering_array):
             self.steering_array = generate_control_points(value_range=(-0.5,0.5))
-            self.speed_array = generate_control_points(value_range=(0,4))
+            self.speed_array = generate_control_points(value_range=(0.5,4))
             self.i=0
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing steering: "%f"' % msg.steering_angle)
